f587383343007f7201da6a48f4e9a59ba4335d13
Evgeny <buriy-anarchist@inbox.ru>
2022-07-10 19:08:41 +0300

    Add task 19

---
+++ task_19/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int n1, n2, n4, n5;
+n1 = n / 10000;
+n2 = (n % 10000) / 1000;
+n4 = (n % 100) / 10;
+n5 = n % 10;
+if (n1 == n5 && n2 == n4)
+{
+    Console.WriteLine("данное число является палиндромом");
+}
+else
+{
+    Console.WriteLine("данное число не является палиндромом");
+}
