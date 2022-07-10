f45bad4267fe7efb9b34048065113a6b297027a0
Evgeny <buriy-anarchist@inbox.ru>
2022-07-10 19:25:18 +0300

    Add task 21

---
+++ task_21/Program.cs
@@ -0,0 +1,11 @@
+﻿double x, y, z, x1, y1, z1, l;
+x = Convert.ToInt32(Console.ReadLine());
+y = Convert.ToInt32(Console.ReadLine());
+z = Convert.ToInt32(Console.ReadLine());
+x1 = Convert.ToInt32(Console.ReadLine());
+y1 = Convert.ToInt32(Console.ReadLine());
+z1 = Convert.ToInt32(Console.ReadLine());
+l = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) + (z - z1) * (z - z1));
+{
+    Console.WriteLine(l);
+}
